014b5ee23f4c11746ecaab4d38eb8968c1d8b4dd
chiehan <chieh.an.hsu@gmail.com>
2018-12-13 12:19:17 +0800

    try use xcode

---
+++ MyFirstProject/MyCustomViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyCustomViewController.swift
+//  MyFirstProject
+//
+//  Created by chiehan on 2018/12/13.
+//  Copyright © 2018 chiehan. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
